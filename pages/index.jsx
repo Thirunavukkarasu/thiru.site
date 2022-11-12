32b3e4d006d1d3d3ae93de974a65ba7ee3be1b9f
Swati Gupta <swati.gupta@byjus.com>
2022-11-12 19:33:59 +0530

    fixed import issues

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
-import ContainerBlock from "../components/ContainerBlock";
-import FavouriteProjects from "../components/FavouriteProjects";
-import LatestCode from "../components/LatestCode";
-import Hero from "../components/Hero";
+import ContainerBlock from "@components/ContainerBlock";
+import FavouriteProjects from "@components/FavouriteProjects";
+import LatestCode from "@components/LatestCode";
+import Hero from "@components/Hero";
 import getLatestRepos from "@lib/getLatestRepos";
 import userData from "@constants/data";
 
