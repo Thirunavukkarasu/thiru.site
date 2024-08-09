58de6bf2fd371d1b8ab0bd126e5677b0e27ded38
Thirunavukkarasu Muthusamy <thiru@mantys.io>
2024-08-09 18:50:09 +0530

    removed old app

--- app/view-counter.tsx
+++
@@ -1,9 +0,0 @@
-import { kv } from '@vercel/kv'
-
-//export const dynamic = 'force-dynamic'
-
-export default async function ViewCounter({ slug }: any) {
-  const views: any = await kv.get(slug)
-
-  return <p>{Intl.NumberFormat('en-us').format(views)} views</p>
-}
